a1b82f7f106f3994dcbd539c5f6fee0ff2ba04ab
Viral Chauhan <viral7chauhan@yahoo.com>
2022-12-13 16:19:59 +0530

    Initial Commit

---
+++ BitCoinPrice/BitCoinPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitCoinPriceApp.swift
+//  BitCoinPrice
+//
+//  Created by Viral on 13/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BitCoinPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
